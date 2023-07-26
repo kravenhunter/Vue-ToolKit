31d762e1a9cc1b30ed71d552587bcda9c5fe5515
kravenhunter <kravenhunter2012@yandex.ru>
2023-07-26 21:22:51 +0300

    Grid commit

--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -1,17 +1,13 @@
 <template>
+  <div class="container">
+    <AddTabs :names="tabs" :selectedTab="selectedTab" @changeTab="changeTab">
+      <div class="page"></div>
+      <Transition name="sl" mode="out-in">
+        <component :is="activeComponent"></component>
+      </Transition>
+    </AddTabs>
 
-      <div class="container">
-          <AddTabs 
-          :names="tabs"
-          :selectedTab="selectedTab"
-          @changeTab="changeTab"> 
-            <div class="page"></div>
-                <Transition name="sl" mode="out-in" >
-                      <component :is="activeComponent"></component>       
-                </Transition>  
-          </AddTabs>
-        
-      <!--       
+    <!--       
             <label>
               <input type="radio" v-model="activeComponent" :value="CompA"> A
             </label>
@@ -21,41 +17,37 @@
             <Transition name="fade" mode="out-in">
                 <component :is="activeComponent"></component>
               </Transition> -->
-
-          
-      </div>
-
+  </div>
 </template>
 
 <script setup>
-import {ref,shallowRef, watchEffect} from 'vue'
+import { ref, shallowRef } from "vue";
 /* import AddCheckboxGroup from '@/components/UI/CheckBoxUI/AddCheckboxGroup.vue' */
-import AddTabs from '@/components/UI/AddTabs.vue'
-import AddAutoFill from '@/components/UI/Grid/Autofill/AddAutoFill.vue'
-import AddPositionsGrid from '@/components/UI/Grid/GridRadio/AddPositionsGrid.vue'
-import AddAbsolutePosGrid from '@/components/UI/Grid/Absolute/AddAbsolutePosGrid.vue'
-import Layout1 from '@/components/UI/Grid/Test/AddTestGrid.vue'
-import Layout2 from '@/components/UI/Grid/Test/AddTestAutofill.vue'
-import TestGridSlot from '@/components/UI/Grid/Test/TestGridSlot.vue'
-import GhildsPosition from '@/components/UI/Grid/Test/ElementPosition.vue'
+import AddTabs from "@/components/UI/AddTabs.vue";
+import AddAbsolutePosGrid from "@/components/UI/Grid/Absolute/AddAbsolutePosGrid.vue";
+import AddAutoFill from "@/components/UI/Grid/Autofill/AddAutoFill.vue";
+import AddPositionsGrid from "@/components/UI/Grid/GridRadio/AddPositionsGrid.vue";
+import RightPositionGrid from "@/components/UI/Grid/Test/AddRightPositionGrid.vue";
+import RightPositionGrid2 from "@/components/UI/Grid/Test/AddRightPositionGrid2.vue";
+import GhildsPosition from "@/components/UI/Grid/Test/ElementPosition.vue";
+import TestGridSlot from "@/components/UI/Grid/Test/TestGridSlot.vue";
 
 const tabs = [
-    {name: 'AddAutoFill', label: 'Grid AutoFill Blocks', comp: AddAutoFill },
-    {name: 'AddPositionsGrid', label: 'Position Group', comp: AddPositionsGrid},
-    {name: 'AddAbsolutePosGrid', label: 'Absolute Position Group', comp: AddAbsolutePosGrid},
-    {name: 'Layout1', label: 'Layout 1', comp: Layout1},
-    {name: 'Layout2', label: 'Layout 2', comp: Layout2},
-    {name: 'GridSlot', label: 'Grid Slot', comp: TestGridSlot},
-    {name: 'GhildsPosition', label: 'Grid Ghilds Position', comp: GhildsPosition}
-  ];
-  const selectedTab = ref('AddAutoFill');
-  const activeComponent = shallowRef(AddAutoFill)
-  const changeTab = (tabName) => {
-    selectedTab.value = tabs.find(c => c.name === tabName).name;
-    activeComponent.value = tabs.find(c => c.name === tabName).comp;
-    console.log(tabName);
-  };
-
+  { name: "AddAutoFill", label: "Grid AutoFill Blocks", comp: AddAutoFill },
+  { name: "AddPositionsGrid", label: "Position Group", comp: AddPositionsGrid },
+  { name: "AddAbsolutePosGrid", label: "Absolute Position Group", comp: AddAbsolutePosGrid },
+  { name: "Right Position Grid", label: "Layout 1", comp: RightPositionGrid },
+  { name: "Right Position Grid 2", label: "Layout 2", comp: RightPositionGrid2 },
+  { name: "GridSlot", label: "Grid Slot", comp: TestGridSlot },
+  { name: "GhildsPosition", label: "Grid Ghilds Position", comp: GhildsPosition },
+];
+const selectedTab = ref("AddAutoFill");
+const activeComponent = shallowRef(AddAutoFill);
+const changeTab = (tabName) => {
+  selectedTab.value = tabs.find((c) => c.name === tabName).name;
+  activeComponent.value = tabs.find((c) => c.name === tabName).comp;
+  console.log(tabName);
+};
 
 /* const selectedVerticalRadioGrid = ref('auto')
 const selectedHorizontRadioGrid = ref('auto') */
@@ -66,7 +58,6 @@ const selectedHorizontRadioGrid = ref('auto') */
   }
    
   ) */
-
 </script>
 
 <style lang="scss" scoped>
@@ -78,11 +69,10 @@ const selectedHorizontRadioGrid = ref('auto') */
 .sl-leave-to {
   opacity: 0;
 }
-.page{
-position: relative;
- &-content{
- 
-   position: absolute ;
- }
+.page {
+  position: relative;
+  &-content {
+    position: absolute;
+  }
 }
 </style>
