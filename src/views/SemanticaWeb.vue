7ec3d9e4b3f77693f0a79e9f0e829d0fab9688ea
kravenhunter <kravenhunter2012@yandex.ru>
2023-09-04 21:03:57 +0300

    Semantica Web

---
+++ src/views/SemanticaWeb.vue
@@ -0,0 +1,142 @@
+<template>
+  <body>
+    <header>
+      <nav>
+        <a href="#"></a>
+        <ul class="menu">
+          <li><a href="#"></a></li>
+        </ul>
+        <a class="phone" href="tel:4954135467">495-413-5467</a>
+      </nav>
+    </header>
+    <main>
+      <section class="promo">
+        <h1>Promo</h1>
+        <p class="text"></p>
+        <a href="#" class="link"></a>
+      </section>
+      <section class="aboy">
+        <h2>О нас</h2>
+        <b class="proposal"></b>
+        <dl>
+          <dt>Twitter</dt>
+          <dd>165.954 - subscribers</dd>
+          <dt>Youtube</dt>
+          <dd>10,954 - subscribers</dd>
+        </dl>
+      </section>
+      <section class="prices">
+        <h2>Тарифы</h2>
+        <b class="proposal"></b>
+        <ul class="prices_list">
+          <li>
+            <h3 class="title">
+              <ol>
+                <li></li>
+                <li></li>
+              </ol>
+            </h3>
+          </li>
+        </ul>
+      </section>
+      <section class="video">
+        <h2>Video title</h2>
+        <video poster="images/poster.jpg">
+          <source src="video/video.mp4" />
+        </video>
+        <button class="play" type="button"></button>
+      </section>
+      <section class="reviews">
+        <h3 class="title">Отзывы</h3>
+        <ul>
+          <li>
+            <blockquote>
+              <img src="images.jpg" alt="image1" class="avatar" />
+              <cite>Алла нтоненко</cite>
+              <time datetime="2021-04-20">20.04.2021</time>
+              <p>отзыв</p>
+            </blockquote>
+          </li>
+          <li>Отзыв 2</li>
+          <li>
+            <blockquote>
+              <img src="images1.jpg" alt="image2" class="avatar" />
+              <cite>Ольга нтоненко</cite>
+              <time datetime="2021-04-20">20.04.2021</time>
+              <p>отзыв</p>
+            </blockquote>
+          </li>
+        </ul>
+        <button><img src="images3.jp" alt="next" />Next</button>
+      </section>
+      <section class="scheduler">
+        <h2>Разписание</h2>
+        <table>
+          <tr>
+            <th>Услуга</th>
+            <th>Стоимость</th>
+          </tr>
+          <tr>
+            <td>
+              <b>Наша услуга</b>
+            </td>
+            <td>200 р.</td>
+          </tr>
+        </table>
+      </section>
+      <section class="blog">
+        <h2>Блог</h2>
+        <article>
+          <h3>Title</h3>
+          <img src="image4.jpg" alt="image4" />
+          <p class="text">sdfgfg</p>
+          <a href="#" class="link">Подробнее</a>
+        </article>
+
+        <article>
+          <h3><a href="#" class="link">Title</a></h3>
+          <img src="image4.jpg" alt="image4" />
+        </article>
+        <article>
+          <a href="#" class="link">
+            <h3>Title</h3>
+            <img src="image4.jpg" alt="image4" />
+          </a>
+        </article>
+        <article>
+          <a href="#" class="link">
+            <img src="image4.jpg" alt="image4" />
+          </a>
+        </article>
+        <a href="#">Go next</a>
+      </section>
+      <section class="feedbask">
+        <h2>Title form</h2>
+        <form action="#">
+          <label for="name">Name</label>
+          <input id="name" type="text" placeholder=" text" />
+
+          <label for="phone">
+            <input id="phone" type="tel" placeholder=" phone" />
+            <p>some text</p>
+          </label>
+          <label for="question ">Name</label>
+          <textarea id="question" placeholder=" question"></textarea>
+          <button type="submit"></button>
+        </form>
+      </section>
+      <sectoin class="contacts">
+        <h2>Contacts</h2>
+        <dl>
+          <dt>Adress</dt>
+          <dd>
+            <address>st. Monreal 4, 15</address>
+          </dd>
+        </dl>
+      </sectoin>
+    </main>
+    <footer>
+      <p>Copyright 2023</p>
+    </footer>
+  </body>
+</template>
